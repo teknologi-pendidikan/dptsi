39cd1bfd0976f1dddc49ca08e59ea6d64de61dac
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-02-28 15:12:51 +0700

    basic auth with AAD

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,73 @@
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./header.module.css";
+
 // @ts-expect-error: Title are string make sureable
 export default function Header({ title }): JSX.Element {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
   return (
-    <>
-      <header className="flex items-center justify-center fixed w-full h-16 z-40 bg-brand-100 shadow-md">
-        <div className="w-full sm:max-w-xl mx-auto flex items-center justify-between h-full px-4">
-          <p className="text-xl text-neutral-200">{title}</p>
-        </div>
-      </header>
-    </>
+    <header>
+      <noscript>
+        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
+      </noscript>
+      <div className={styles.signedInStatus}>
+        <p
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
+          {!session && (
+            <>
+              <span className={styles.notSignedInText}>
+                You are not signed in
+              </span>
+              <a
+                href={`/api/auth/signin`}
+                className={styles.buttonPrimary}
+                onClick={(e) => {
+                  e.preventDefault()
+                  signIn()
+                }}
+              >
+                Sign in
+              </a>
+            </>
+          )}
+          {session?.user && (
+            <>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                  className={styles.avatar}
+                />
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email ?? session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                className={styles.button}
+                onClick={(e) => {
+                  e.preventDefault()
+                  signOut()
+                }}
+              >
+                Sign out
+              </a>
+            </>
+          )}
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center fixed w-full h-16 z-40 bg-brand-100 shadow-md">
+      <div className="w-full sm:max-w-xl mx-auto flex items-center justify-between h-full px-4">
+        <p className="text-xl text-neutral-200">{title}</p>
+      </div>
+      </div>
+    </header>
   );
 }
