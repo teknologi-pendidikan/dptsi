1b4685ebbc5a310a8b6554ef70fb9f5f80a536d9
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-02-28 15:51:21 +0700

    add login status display on header

--- components/header.tsx
+++ components/header.tsx
@@ -12,61 +12,35 @@ export default function Header({ title }): JSX.Element {
       <noscript>
         <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
       </noscript>
-      <div className={styles.signedInStatus}>
-        <p
-          className={`nojs-show ${
-            !session && loading ? styles.loading : styles.loaded
-          }`}
-        >
-          {!session && (
-            <>
-              <span className={styles.notSignedInText}>
-                You are not signed in
-              </span>
-              <a
-                href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signIn()
-                }}
-              >
-                Sign in
-              </a>
-            </>
-          )}
-          {session?.user && (
-            <>
-              {session.user.image && (
-                <span
-                  style={{ backgroundImage: `url('${session.user.image}')` }}
-                  className={styles.avatar}
-                />
-              )}
-              <span className={styles.signedInText}>
-                <small>Signed in as</small>
-                <br />
-                <strong>{session.user.email ?? session.user.name}</strong>
-              </span>
-              <a
-                href={`/api/auth/signout`}
-                className={styles.button}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signOut()
-                }}
-              >
-                Sign out
-              </a>
-            </>
-          )}
-        </p>
-      </div>
 
       <div className="flex items-center justify-center fixed w-full h-16 z-40 bg-brand-100 shadow-md">
-      <div className="w-full sm:max-w-xl mx-auto flex items-center justify-between h-full px-4">
-        <p className="text-xl text-neutral-200">{title}</p>
-      </div>
+        <div className="w-full sm:max-w-xl mx-auto flex items-center justify-between h-full px-4">
+          <p className="text-xl text-neutral-200">{title}</p>
+          <span>
+            {!session && (
+              <>
+                <a
+                  className="text-lg text-neutral-200"
+                  href={"/api/auth/signin"}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signIn();
+                  }}
+                >
+                  You are not signed in
+                </a>
+              </>
+            )}
+            {session?.user && (
+              <a className="text-lg text-neutral-200"
+              href={"/api/auth/signout"}
+              onClick={(e) => {
+                e.preventDefault();
+                signOut();
+              }}>Hello {session.user.email ?? session.user.name}</a>
+            )}
+          </span>
+        </div>
       </div>
     </header>
   );
